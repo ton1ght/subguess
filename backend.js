546823902f325df13e13262cd2996f832616c2ce
Stuxnot <41650734+Stuxnot@users.noreply.github.com>
2021-08-25 17:58:46 +0200

    Refactors getPost

--- backend.js
+++ backend.js
@@ -7,6 +7,21 @@ var lifes = 5
 var postCount = 0;
 var redditData = [];
 
+function contains_all(str, search) {
+    ret = 0;
+    search.forEach(word => ret += str.includes(word));
+    return (ret === search.length);
+}
+
+function contains_one(str, search) {
+    for (const word of search) {
+        if (str.includes(word)) {
+            return true;
+        }
+    }
+    return false;
+}
+
 function getPost(debug_index = null) {
     hint = 0;
 
@@ -24,64 +39,60 @@ function getPost(debug_index = null) {
     document.getElementById("skips").innerHTML = skips;
     document.getElementById("title").innerHTML = redditData[index].title;
 
-    if (redditData[index].desc != "" && !redditData[index].desc.includes("https")) {
-        document.getElementById("description").innerHTML = redditData[index].desc;
+    let element = redditData[index];
+
+    if (element.desc != "") {
+        document.getElementById("description").innerHTML = element.desc;
     }
-    // images
-    if (redditData[index].url.includes("i.redd.it") || (redditData[index].url.includes("imgur") && !redditData[index].url.includes("gifv"))) {
-        var x = document.createElement("img");
-        x.setAttribute("src", redditData[index].url);
-        x.setAttribute("onerror","this.style.display='none'");
+
+    if (contains_one(element.vid, ["v.redd.it", ".gifv"])) {
+        let video_url = element.url.includes("v.redd.it") ? element.vid + ".gif" : element.url.replace(".gifv", ".mp4");
+        let video = document.createElement('video');
+        video.src = element.vid;
+        video.type = "video/mp4";
+        video.autoplay = true;
+        video.muted = true;
+        video.loop = true;
+        document.getElementById("placeholder").appendChild(video);
+
+    } else if (contains_one(element.url, ["i.redd.it", "imgur"])) {
+        let x = document.createElement("img");
+        x.setAttribute("src", element.url);
+        x.setAttribute("onerror", "this.style.display='none'");
+
         document.getElementById("placeholder").appendChild(x);
-    // gfycat
-    } else if (redditData[index].url.includes("gfycat")) {
-        var video = document.createElement('video');
-        video.src = redditData[index].vid + ".gif";
-        video.type = "video/mp4";
-        video.autoplay = true;
-        video.muted = true;
-        video.loop = true;
-        document.getElementById("placeholder").appendChild(video);
-    // videos
-    } else if (redditData[index].url.includes("v.redd.it")) {
-        var video = document.createElement('video');
-        video.src = redditData[index].vid;
-        video.type = "video/mp4";
-        video.autoplay = true;
-        video.muted = true;
-        video.loop = true;
-        document.getElementById("placeholder").appendChild(video);
-    // gifs
-    } else if (redditData[index].url.includes(".gifv")) {
-        newString = redditData[index].url.replace(".gifv", ".mp4");
-        var video = document.createElement('video');
-        video.src = newString;
-        video.type = "video/mp4";
-        video.autoplay = true;
-        video.loop = true;
-        video.muted = true;
-        document.getElementById("placeholder").appendChild(video);
-    // text
-    } else if (redditData[index].url.includes("twitter")) {
-        link = "https://twitframe.com/show?url=" + encodeURI(redditData[index].url);
+
+    } else if (element.url.includes("twitter")) {
+        let iframe_url = "https://twitframe.com/show?url=" + encodeURI(element.url);
+
         var hyperlink = document.createElement('a');
-        hyperlink.href = link;
-        hyperlink.innerText = redditData[index].url;
+        hyperlink.href = element.url;
+        hyperlink.innerText = element.url;
+        hyperlink.target = "_blank";
         document.getElementById("link").appendChild(hyperlink);
 
         let ifrm = document.createElement('iframe');
-        ifrm.setAttribute("src", link);
+        ifrm.setAttribute("src", iframe_url);
         document.getElementById("placeholder").appendChild(ifrm);
 
-    } else if(!redditData[index].url.includes("reddit")) {
+    } else if (element.url.includes("gfycat") || contains_all(element.url, ["gallery", "reddit"])) {
+        document.getElementById("placeholder").innerHTML = "We can not display the media at this time.";
         var hyperlink = document.createElement('a');
-        hyperlink.href = redditData[index].url;
-        hyperlink.innerText = redditData[index].url;
+        hyperlink.href = element.url;
+        hyperlink.innerText = element.url;
+        hyperlink.target = "_blank";
         document.getElementById("link").appendChild(hyperlink);
 
-        let ifrm = document.createElement('iframe');
-        ifrm.setAttribute("src", redditData[index].url);
-        document.getElementById("placeholder").appendChild(ifrm);
+    } else if (element.url != "" && !element.url.includes("reddit")) {
+        var hyperlink = document.createElement('a');
+        hyperlink.href = element.url;
+        hyperlink.innerText = element.url;
+        hyperlink.target = "_blank";
+        document.getElementById("link").appendChild(hyperlink);
+
+    } else {
+        document.getElementById("description").innerHTML = "";
+        document.getElementById("placeholder").innerHTML = element.desc;
     }
 
     return index;
